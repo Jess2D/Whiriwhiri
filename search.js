796fcb4e5134c5f8c17101c947d0b1338b96b654
Jessica Silveira <jessicap_silveira@hotmail.com>
2022-06-21 06:33:22 +1200

    Add search section

---
+++ search.js
@@ -0,0 +1,34 @@
+const accomodation = [
+  {
+    type: "Hotel",
+    "min-people": "1",
+    "max-people": "2",
+    price: "$157/night",
+    "min-night": "1",
+    "max-night": "5",
+  },
+  {
+    type: "Hostel",
+    "min-people": "1",
+    "max-people": "1",
+    price: "$30/night",
+    "min-night": "1",
+    "max-night": "10",
+  },
+  {
+    type: "Motel",
+    "min-people": "2",
+    "max-people": "4",
+    price: "$90/night",
+    "min-night": "3",
+    "max-night": "10",
+  },
+  {
+    type: "House",
+    "min-people": "1",
+    "max-people": "4",
+    price: "$240/night",
+    "min-night": "2",
+    "max-night": "15",
+  },
+];
