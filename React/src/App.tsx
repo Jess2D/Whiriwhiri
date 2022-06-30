e15091807c08af98ac0aab7995517055146c53e9
Danilo <danilomourag@gmail.com>
2022-06-30 20:24:36 +1200

    Add base React layout

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,26 +1,159 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+
+import { Button, Card, Col, Container, Form, ListGroup, Row, Stack } from 'react-bootstrap';
+import styled from "styled-components";
+import { accomodatio } from './data';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Stack>
+
+        <StyledTopSection>
+          <img src={logo} alt="Logo" width={"300px"} />
+          <Container>
+            <Form>
+              <StyledMainInputRow>
+                <Row>
+                  <Col>
+                    <Form.Group className="mb-6" controlId="formLocation">
+                      <Form.Control type="text" placeholder="Location" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className="mb-6" controlId="formCheckIn">
+                      <Form.Control type="date" name="dob" placeholder="Check-In Date" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className="mb-6" controlId="formCheckOut">
+                      <Form.Control type="date" name="dob" placeholder="Check-Out Date" />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group className="mb-6" controlId="formNumberPeople" placeholder='Number of people'>
+                      <Form.Select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                      </Form.Select>
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <div className="d-grid gap-2">
+                      <Button className="mb-6" variant="primary" type="submit">
+                        Search
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
+              </StyledMainInputRow>
+              <Row>
+              </Row>
+            </Form>
+          </Container>
+        </StyledTopSection>
+
+        <StyledFilterSection>
+          <Form>
+            <Container>
+              <Row>
+                <Col>
+                  <Form.Group className="mb-6" controlId="formNumberPeople" placeholder='Accomodation type'>
+                    <Form.Select>
+                      <option>Filter by type</option>
+                      <option>Hotel</option>
+                      <option>Hostel</option>
+                      <option>Motel</option>
+                      <option>House</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group className="mb-6" controlId="formNumberPeople" placeholder='Price range'>
+                    <Form.Select>
+                      <option>Filter by price</option>
+                      <option>Less than $50</option>
+                      <option>Between $50 - $100</option>
+                      <option>Between $100 - $200</option>
+                      <option>Between $200 - $300</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col></Col>
+                <Col></Col>
+                <Col></Col>
+              </Row>
+            </Container>
+          </Form>
+        </StyledFilterSection>
+
+        <StyledContentSection>
+          <Container>
+            <Row>
+              <Form.Label column="lg" style={{ textAlign: "left" }}><b>Results</b></Form.Label>
+            </Row>
+            <Row xs={1} md={4} className="g-4">
+              {accomodatio.map(item => (
+                <Col>
+                  <Card>
+                    <Card.Img variant="top" src={item.image} />
+                    <Card.Body>
+                      <Card.Title>{item.type}</Card.Title>
+                      <Card.Text>
+                        <ListGroup variant='flush'>
+                          <ListGroup.Item>{item.minNight} to {item.maxNight} nights</ListGroup.Item>
+                          <ListGroup.Item>{item.minPeople} to {item.maxPeople} guests</ListGroup.Item>
+                          <ListGroup.Item><b>{item.price}</b> per night</ListGroup.Item>
+                        </ListGroup>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </StyledContentSection>
+
+
+      </Stack>
+
+    </StyledApp >
   );
 }
 
 export default App;
+
+const StyledApp = styled.div`
+  text-align: center;
+`;
+
+const StyledTopSection = styled.div`
+  background-color: #061826;
+  padding: 1rem;
+`;
+
+const StyledFilterSection = styled.div`
+  background-color: #b5b5b5;
+  padding: 1rem;
+  height: 100%;
+`;
+
+const StyledContentSection = styled.div`
+  background-color: #ffffff;
+  padding: 1rem;
+  height: 100%;
+`;
+
+const StyledMainInputRow = styled.div`
+  background-color: #357088;
+  padding: .5rem;
+  border-radius: .2rem;
+`;
---
+++ React/src/data.ts
@@ -0,0 +1,22 @@
+import hostel from './types/hostel.png'
+import hotel from './types/hotel.png'
+import house from './types/house.png'
+import motel from './types/motel.png'
+
+interface Accomodation {
+    image: string;
+    type: string;
+    minPeople: number;
+    maxPeople: number;
+    minNight: number;
+    maxNight: number;
+    price: string;
+}
+
+export const accomodatio: Accomodation[] =
+    [
+        { image: hotel, type: "Hotel", minPeople: 1, maxPeople: 2, price: "$157/night", minNight: 1, maxNight: 5 },
+        { image: hostel, type: "Hostel", minPeople: 1, maxPeople: 1, price: "$30/night", minNight: 1, maxNight: 10 },
+        { image: motel, type: "Motel", minPeople: 2, maxPeople: 4, price: "$90/night", minNight: 3, maxNight: 10 },
+        { image: house, type: "House", minPeople: 1, maxPeople: 4, price: "$240/night", minNight: 2, maxNight: 15 }
+    ]
